b5ad88b15624bc96d320d086e592ff81388f8ba7
Eugene Eeo <xe0jun@hotmail.com>
2014-12-28 15:08:24 +0800

    refactor

--- critto/objects.py
+++ critto/objects.py
@@ -2,24 +2,11 @@ from json import loads
 from critto.ropt import ROpt
 
 
+NAME = r'(\w[$\w0-9_]*)'
+
+
 def ignore_ws(regex):
     return '\W*%s\W*' % regex
-
-
-class Name(ROpt):
-    regex = r'(\w[$\w0-9_]*)'
-
-    def __call__(self, ctx, match):
-        name = match.group(1)
-        return name in ctx.conds
-
-
-class Cond(ROpt):
-    regex = r'%s=(.+?)' % Name.regex
-
-    def __call__(self, ctx, match):
-        name, value = match.groups()
-        return ctx.conds[name]() == loads(value)
 
 
 class Any(ROpt):
@@ -31,34 +18,37 @@ class Any(ROpt):
 
 
 class Flag(ROpt):
-    regex = ignore_ws(r'#\[%s\]' % Name.regex)
+    regex = ignore_ws(r'#\[%s\]' % NAME)
 
     def __call__(self, ctx, match):
         name = match.group(1)
         return ctx.flags[name]()
 
 
-class IfName(Name):
-    regex = ignore_ws(r'#\[if %s\]' % Name.regex)
+class IfName(ROpt):
+    regex = ignore_ws(r'#\[if %s\]' % NAME)
 
     def __call__(self, ctx, match):
         if ctx.stack[-1]:
-            val = Name.__call__(self, ctx, match)
-            ctx.stack.append(val)
-
-
-class IfCond(Cond):
-    regex = ignore_ws(r'#\[if %s\]' % Cond.regex)
-
-    def __call__(self, ctx, match):
-        if ctx.stack[-1]:
-            val = Cond.__call__(self, ctx, match)
-            ctx.stack.append(val)
+            res = match.group(1) in ctx.conds
+            ctx.stack.append(res)
         else:
             ctx.stack.append(False)
 
 
-class EndBlock(Cond):
+class IfCond(ROpt):
+    regex = ignore_ws(r'#\[if %s=(.+?)\]' % NAME)
+
+    def __call__(self, ctx, match):
+        if ctx.stack[-1]:
+            name, value = match.groups()
+            res = ctx.conds[name]() == loads(value)
+            ctx.stack.append(res)
+        else:
+            ctx.stack.append(False)
+
+
+class EndBlock(ROpt):
     regex = ignore_ws(r'#\[end(if|block)\]')
 
     def __call__(self, ctx, match):
